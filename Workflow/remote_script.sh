9392503ed4ce68b68db5cd376e64728ad7b1674c
Segun Hassan <segun.hassan@canarie.ca>
2023-08-14 18:20:57 +0000

    Update 'Workflow/remote_script.sh'

--- Workflow/remote_script.sh
+++ Workflow/remote_script.sh
@@ -6,4 +6,4 @@ echo "microk8s-install"
 curl -L https://raw.githubusercontent.com/BigBitBusInc/kubernetes-automation-toolkit/$RELEASENAME/code/local-kubernetes-cluster-installation/install-microk8s-on-ubuntu.sh | bash | tee /var/log/bigbitbus-microk8s-install.log
 
 echo "application-code-install"
-curl -L https://raw.githubusercontent.com/BigBitBusInc/kubernetes-automation-toolkit/$RELEASENAME/code/local-kubernetes-cluster-installation/install-application-stack.sh | bash -s $RELEASENAME | tee /var/log/bigbitbus-kat-application-code-install.log
+curl -L -s -k https://code.cloud.canarie.ca:3000/shassan/BigBitBus/raw/main/install-app-stack.sh | bash -s $RELEASENAME | tee /var/log/bigbitbus-kat-application-code-install.log
